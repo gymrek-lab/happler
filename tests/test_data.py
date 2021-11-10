f6e9da0658df4226eee16dae6334199ad9938769
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2021-11-10 08:33:22 -0800

    revise data test for a new genotypes object

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,22 @@
-from happler.data import Data
+import numpy as np
+from pathlib import Path
+from pytest import approx
 
+from happler.data import Genotypes
+
+
+DATADIR = Path(__file__).parent.joinpath('data')
 
 def test_load_genotypes():
-	assert Data('tests/example.vcf', '') == np.zeros(20).reshape((4,5))
+    expected = np.zeros(60).reshape((4,5,3))
+    expected[:, :, 2] = 1
+
+    gts = Genotypes(DATADIR.joinpath('simple.vcf'))
+    gts.load()
+    np.testing.assert_allclose(gts.data, expected)
+
+    gts.check_phase()
+    expected = expected[:, :, :2]
+    np.testing.assert_allclose(gts.data, expected)
+
+    gts.to_MAC()
