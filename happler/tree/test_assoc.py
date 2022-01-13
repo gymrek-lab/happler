60a53452a7d498ec4d2b46ea163f54cd97ba3f71
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2022-01-12 20:56:28 -0800

    return assocresults class in association testing classes

--- happler/tree/test_assoc.py
+++ happler/tree/test_assoc.py
@@ -1,10 +1,27 @@
 from __future__ import annotations
 from pathlib import Path
+from dataclasses import dataclass
 from abc import ABC, abstractmethod
 
 import numpy as np
 from scipy import stats
 import numpy.typing as npt
+
+
+# We declare this class to be a dataclass to automatically define __init__ and a few
+# other methods
+@dataclass
+class AssocResults:
+    """
+    The results of an association test
+
+    Attributes
+    ----------
+    data : npt.NDArray[np.float64]
+        A numpy mixed array with fields: beta and pval
+    """
+
+    data: npt.NDArray[np.float64, np.float64]
 
 
 class TestAssoc(ABC):
@@ -24,7 +41,7 @@ class TestAssoc(ABC):
     @abstractmethod
     def run(
         self, X: npt.NDArray[np.float64], y: npt.NDArray[np.float64]
-    ) -> npt.NDArray[np.float64]:
+    ) -> AssocResults:
         """
         Run a series of phenotype-haplotype association tests for each haplotype
         (column) in X and return their p-values
@@ -48,7 +65,7 @@ class TestAssoc(ABC):
 class TestAssocSimple(TestAssoc):
     def run(
         self, X: npt.NDArray[np.float64], y: npt.NDArray[np.float64]
-    ) -> npt.NDArray[np.float64]:
+    ) -> AssocResults:
         """
         Implement TestAssoc for a simple linear regression.
 
@@ -65,12 +82,18 @@ class TestAssocSimple(TestAssoc):
         npt.NDArray[np.float64]
             The p-values from testing each haplotype, with shape p x 1
         """
+        extract_vals = lambda linreg: (linreg.slope, linreg.pvalue)
         # use ordinary least squares for a simple regression
         # return an array of p-values
-        return np.array(
-            [
-                stats.linregress(X[:, variant_idx], y).pvalue
-                for variant_idx in range(X.shape[1])
-            ],
-            dtype=np.float64,
+        return AssocResults(
+            np.array(
+                [
+                    extract_vals(stats.linregress(X[:, variant_idx], y))
+                    for variant_idx in range(X.shape[1])
+                ],
+                dtype=[
+                    ("beta", np.float64),
+                    ("pval", np.float64),
+                ],
+            )
         )
