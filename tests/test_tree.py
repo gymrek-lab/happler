ac08e9e8af13988c81021743eea54de38b90f500
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2022-01-12 20:59:54 -0800

    test new haplotypes alg

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -49,9 +49,39 @@ def test_tree():
 
     assert tree.num_nodes == 1
 
-    tree.add_node(Variant(idx=0, id="SNP1", pos=2), 0, 0)
+    tree.add_node(Variant(idx=1, id="SNP1", pos=2), 0, 0)
 
     assert tree.num_nodes == 2
+
+
+def test_get_haplotypes_from_tree():
+    # create three new nodes
+    snp1 = Variant(idx=0, id="SNP1", pos=1)
+    snp2 = Variant(idx=1, id="SNP2", pos=2)
+    snp3 = Variant(idx=2, id="SNP3", pos=3)
+
+    # create a tree composed of just one node
+    tree = Tree(root=snp1)
+    haps = tree.haplotypes()
+    assert len(haps) == 1
+    assert haps[0][0]["variant"] == snp1
+
+    # add two child nodes
+    snp2_idx = tree.add_node(snp2, parent_idx=0, allele=0)
+    tree.add_node(snp2, parent_idx=0, allele=1)
+    haps = tree.haplotypes()
+    assert len(haps) == 2
+    assert tuple([len(hap) for hap in haps]) == (2, 2)
+    assert [haps[0][i]["variant"] for i in range(2)] == [snp1, snp2]
+    assert [haps[1][i]["variant"] for i in range(2)] == [snp1, snp2]
+
+    # add a single child to the first leaf
+    tree.add_node(snp3, parent_idx=snp2_idx, allele=1)
+    haps = tree.haplotypes()
+    assert len(haps) == 2
+    assert tuple([len(hap) for hap in haps]) == (3, 2)
+    assert [haps[0][i]["variant"] for i in range(3)] == [snp1, snp2, snp3]
+    assert [haps[1][i]["variant"] for i in range(2)] == [snp1, snp2]
 
 
 def test_tree_builder():
@@ -60,6 +90,7 @@ def test_tree_builder():
     tree_builder = TreeBuilder(gens, phens)
     root_node = 0
     tree_builder.run(root_node)
+    # TODO: assert that the tree looks correct
 
 
 def test_haplotype():
@@ -86,21 +117,6 @@ def test_haplotype_transform():
     hap = Haplotype.from_node(variant, allele_idx, variant_gts)
     gens_without_variant = gens.data[:, (variant_idx + 1) :, :]
     np.testing.assert_allclose(hap.transform(gens), gens_without_variant)
-
-
-def test_haplotypes():
-    gens = Genotypes.load(DATADIR.joinpath("simple.vcf"))
-    haps = Haplotypes(gens)
-
-    variant = Variant.from_np(gens.variants[0], 0)
-    haps.add_variant(variant, 0)
-    np.testing.assert_allclose(gens.data[:, 0, 0][:, np.newaxis], haps.data)
-
-    variant = Variant.from_np(gens.variants[1], 1)
-    haps.add_variant(variant, 0)
-    np.testing.assert_allclose(
-        np.logical_and(gens.data[:, 0, 0], gens.data[:, 1, 0])[:, np.newaxis], haps.data
-    )
 
 
 def test_simple_assoc():
