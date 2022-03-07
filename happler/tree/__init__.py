281b1acbdc839f199bdef352a58175bf63fbc3bb
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2022-03-06 20:34:43 -0800

    run pytest on test_tree

--- happler/tree/__init__.py
+++ happler/tree/__init__.py
@@ -1,6 +1,6 @@
 from .tree_builder import TreeBuilder
 from .variant import VariantType, Variant
 from .haplotypes import Haplotype, Haplotypes
-from .terminate import Terminator, TTestTerminator
+from .terminator import Terminator, TTestTerminator
 from .assoc_test import AssocTest, AssocTestSimple
 from .tree import Tree, NodeResults, NodeResultsExtra
