9e18189dc50f811a68199be32885062dba2a0abb
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2022-03-27 05:38:06 -0700

    add logging to run cli

--- happler/__main__.py
+++ happler/__main__.py
@@ -2,25 +2,30 @@
 
 import sys
 import click
+import logging
 from pathlib import Path
+from typing import TextIO
 from typing import Union, Tuple
 
 from . import tree
 from haptools import data
 
 
-@click.group()
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
 @click.version_option()
 def main():
     """
     happler: A haplotype-based fine-mapping method
 
-    Test for associations between a trait and haplotypes (ie sets of correlated SNPs rather than individual SNPs)
+    Test for associations between a trait and haplotypes (ie sets of correlated SNPs) rather than individual SNPs
     """
     pass
 
 
-@main.command()
+@main.command(context_settings=CONTEXT_SETTINGS)
 @click.argument("genotypes", type=click.Path(exists=True, path_type=Path))
 @click.argument("phenotypes", type=click.Path(exists=True, path_type=Path))
 @click.option(
@@ -58,8 +63,17 @@ def main():
     "-o",
     "--output",
     type=click.File("w"),
+    default="-",
     show_default="stdout",
     help="A .hap file describing the extracted haplotypes.",
+)
+@click.option(
+    "-v",
+    "--verbosity",
+    type=click.Choice(["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG", "NOTSET"]),
+    default="ERROR",
+    show_default="only errors",
+    help="The level of verbosity desired",
 )
 def run(
     genotypes: Path,
@@ -67,14 +81,18 @@ def run(
     region: str = None,
     samples: Tuple[str] = tuple(),
     samples_file: Path = None,
-    output: Path = sys.stdout,
+    output: TextIO = sys.stdout,
+    verbosity: str = 'CRITICAL',
 ):
     """
     Use the tool to find trait-associated haplotypes
 
     GENOTYPES must be formatted as VCFs and
+
     PHENOTYPES must be a tab-separated file containing two columns: sample ID and
     phenotype value
+
+    Ex: happler run tests/data/simple.vcf tests/data/simple.tsv > simple.hap
 
     \f
     Parameters
@@ -82,7 +100,7 @@ def run(
     genotypes : Path
         The path to the genotypes in VCF format
     phenotypes : Path
-        The path to the phenotypes in TSV format
+        The path to the phenotypes in TSV format. There should be no header lines.
     region : str, optional
         See documentation for :py:meth:`~.data.Genotypes.read`
     sample : Tuple[str], optional
@@ -91,6 +109,11 @@ def run(
         A single column txt file containing a list of the samples (one per line) to
         subset from the genotypes file
     """
+    log = logging.getLogger("run")
+    logging.basicConfig(
+        format="[%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)",
+        level=verbosity,
+    )
     # handle samples
     if samples and samples_file:
         raise click.UsageError(
@@ -105,9 +128,13 @@ def run(
     else:
         samples = None
     # load data
+    log.info("Loading genotypes")
     gt = data.Genotypes.load(genotypes, region=region, samples=samples)
+    log.info("Loading phenotypes")
     ph = data.Phenotypes.load(phenotypes, samples=samples)
+    log.info("Running tree builder")
     hap_tree = tree.TreeBuilder(gt, ph).run()
+    log.info("Outputting haplotypes")
     tree.Haplotypes.from_tree(hap_tree).write(output)
 
 
