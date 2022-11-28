1fd172d248ee15256e53c9aac7283e05fa450209
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2022-11-28 07:08:36 -0800

    create script to convert pheno file to phens file

---
+++ analysis/workflow/scripts/pheno2phens.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import click
+import numpy as np
+from pathlib import Path
+
+import haptools
+from haptools.data import Data, GenotypesPLINK, Phenotypes
+
+
+@click.command()
+@click.argument("hps", type=click.Path(exists=True, path_type=Path))
+@click.argument("pheno", type=click.Path(exists=True, path_type=Path))
+@click.option(
+    "-o",
+    "--output",
+    type=click.Path(path_type=Path),
+    default=Path("/dev/stdout"),
+    show_default="stdout",
+    help="A TSV file containing simulated phenotypes",
+)
+def main(hps: Path, pheno: Path, output: Path):
+    """
+    Create a phens file for a causal haplotype
+
+    \f
+    Parameters
+    ----------
+    hps: Path
+        The path to the pgen file containing the haplotype pseudogenotypes
+    pheno: Path
+        A file containing the pheno file for the causal haplotype
+    output: Path
+        The path to the output phens file
+    """
+    gts = GenotypesPLINK.load(hps)
+    pts = Phenotypes.load(pheno)
+
+    assert len(gts.variants) == 1
+    assert gts.samples == pts.samples
+    assert len(pts.names) == 1
+    pos = gts.variants["pos"]
+    
+    hap_gts = gts.data[:, 0, :].sum(axis=1)
+    # normalize the genotypes
+    hap_gts = (hap_gts - hap_gts.mean()) / hap_gts.std()
+
+    with Data.hook_compressed(output, mode="w") as phens:
+        phens.write(f"sample\t{pos}:2\tphen")
+        for samp, gt, pt in zip(gts.samples, hap_gts, pts.data[:, 0]):
+            phens.write(f"{samp}, {gt:.17f}, {pt:.17f}")
+
+
+if __name__ == "__main__":
+    main()
