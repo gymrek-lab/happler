80cc708e4eaad744404905d968c3a50708bd9f67
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2022-12-12 14:18:33 -0800

    use proper positions in hap file

--- analysis/workflow/scripts/create_hap_file.sh
+++ analysis/workflow/scripts/create_hap_file.sh
@@ -8,7 +8,8 @@ echo -e "#H\tbeta\t.2f\tEffect size in linear model" >> "${snakemake_output[hap]
 echo -e "H\t${snakemake_params[1]}\t${snakemake_params[2]}\thap\t${snakemake_params[3]}" >> "${snakemake_output[hap]}"
 
 for snp in ${snakemake_params[4]}; do
-    # TODO: figure out how to use the proper start and end positions
-    echo -ne "V\thap\t${snakemake_params[2]}\t" >> "${snakemake_output[hap]}"
-    echo "$snp" | sed 's/\(.*\):/\1\t/' >> "${snakemake_output[hap]}"
+    snp_id="$(echo "$snp" | sed 's/\(.*\):/\1\t/')"
+    start_pos="$(grep -m1 "$(echo "$snp_id" | cut -f1)" "${snakemake_input[gts]}" | cut -f2)"
+    end_pos=$((start_pos+1))
+    echo -e "V\thap\t$start_pos\t$end_pos\t$snp_id" >> "${snakemake_output[hap]}"
 done
