8df1422a5760fa1b6096cc7d5be9172ce0f27f3e
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2022-12-14 15:03:54 -0800

    fix position in create_hap_file

--- analysis/workflow/scripts/create_hap_file.sh
+++ analysis/workflow/scripts/create_hap_file.sh
@@ -5,11 +5,20 @@ exec 2> "${snakemake_log}"
 echo -e "#\torderH\tbeta" > "${snakemake_output[hap]}"
 echo -e "#\tversion\t0.1.0" >> "${snakemake_output[hap]}"
 echo -e "#H\tbeta\t.2f\tEffect size in linear model" >> "${snakemake_output[hap]}"
-echo -e "H\t${snakemake_params[1]}\t${snakemake_params[2]}\thap\t${snakemake_params[3]}" >> "${snakemake_output[hap]}"
 
+starts=()
+ends=()
 for snp in ${snakemake_params[4]}; do
     snp_id="$(echo "$snp" | sed 's/\(.*\):/\1\t/')"
     start_pos="$(grep -m1 "$(echo "$snp_id" | cut -f1)" "${snakemake_input[gts]}" | cut -f2)"
     end_pos=$((start_pos+1))
     echo -e "V\thap\t$start_pos\t$end_pos\t$snp_id" >> "${snakemake_output[hap]}"
+    starts+=("$start_pos")
+    ends+=("$end_pos")
 done
+
+start_pos="$(echo "${starts[@]}" | tr ' ' '\n' | sort -n | head -n1)"
+end_pos="$(echo "${ends[@]}" | tr ' ' '\n' | sort -rn | head -n1)"
+echo -e "H\t${snakemake_params[1]}\t$start_pos\t$end_pos\thap\t${snakemake_params[3]}" >> "${snakemake_output[hap]}"
+
+LC_ALL=C sort -k1,4 -o "${snakemake_output[hap]}" "${snakemake_output[hap]}"
