6c6090157ee8769a5658017fff2d5948395fd97d
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2023-08-03 12:33:38 -0700

    enable splitting of branches as optional

--- happler/tree/tree_builder.py
+++ happler/tree/tree_builder.py
@@ -61,6 +61,10 @@ class TreeBuilder:
         else:
             self.results_type = NodeResults
         self.tree = None
+        self._split_method = self._find_split_rigid
+        self.ld_prune_thresh = ld_prune_thresh
+        if self.ld_prune_thresh is not None:
+            self._split_method = self._find_split_flexible
         self.log = log or getLogger(self.__class__.__name__)
 
     def __repr__(self):
@@ -81,6 +85,9 @@ class TreeBuilder:
         parent_hap = Haplotype(num_samples=len(self.gens.samples))
         # step three: create the rest of the tree
         self._create_tree(parent_hap, parent_idx=0)
+        # step four: prune nodes from the tree that are in strong LD
+        if self._split_method == self._find_split_flexible:
+            self.prune_tree()
         return self.tree
 
     def _create_tree(
@@ -116,7 +123,7 @@ class TreeBuilder:
             )
         )
         # find the variant-allele pairs that give the best haplotype
-        for variant, allele, results in self._find_split(parent_hap, parent_res):
+        for variant, allele, results in self._split_method(parent_hap, parent_res):
             if variant is None:
                 # there were no significant variants!
                 continue
@@ -219,6 +226,92 @@ class TreeBuilder:
             best_variant = Variant.from_np(self.gens.variants[best_var_idx], best_var_idx)
             self.log.debug("Chose variant {}".format(best_variant.id))
             # step 6: check if this allele is significant and whether we should terminate the branch
+            self.log.debug(
+                "Testing variant {} / allele {} with parent_res {} and node_res {}"
+                .format(best_variant.id, allele, parent_res, node_res)
+            )
+            if self.terminator.check(parent_res, node_res, num_samps, num_tests):
+                yield None, allele, node_res
+                continue
+            yield best_variant, allele, node_res
+
+    def _find_split_rigid(
+        self, parent: Haplotype, parent_res: NodeResults = None
+    ) -> tuple[Variant, np.void]:
+        """
+        Find the variant/allele that best fits under the parent_idx node
+
+        Unlike, :py:meth:`~.TreeBuilder._find_split_flexible()`, this method
+        artificially restricts the two branches from a node to representing an allele
+        from the SAME SNP, instead of potentially different SNPs.
+
+        Parameters
+        ----------
+        parent : Haplotype
+            The haplotype containing all variants up to (and including) the parent
+        parent_res : NodeResults, optional
+            The results of the tests performed on the parent node
+
+        Returns
+        -------
+        tuple[Variant, float]
+            The variant that best fits under the parent node with the allele edge AND
+            the results (ex: beta, pval) of the haplotype association test after
+            incorporating that variant
+        """
+        num_samps = len(self.gens.samples)
+        results = {}
+        best_p_idx = {}
+        # iterate through the two possible alleles and try all SNPs with that allele
+        alleles = (0, 1)
+        for allele in alleles:
+            # step 1: transform the GT matrix into a haplotype matrix
+            hap_matrix = parent.transform(self.gens, allele)
+            if hap_matrix.shape[1] == 0:
+                # if there weren't any genotypes left, just return None
+                yield None, allele, None
+                continue
+            # step 2: run all association tests on all of the haplotypes
+            if isinstance(self.method, AssocTestSimpleSMTScore):
+                results[allele] = self.method.run(
+                    hap_matrix.sum(axis=2),
+                    self.phens.data[:, 0],
+                    parent_res=parent_res,
+                )
+            else:
+                results[allele] = self.method.run(
+                    hap_matrix.sum(axis=2),
+                    self.phens.data[:, 0],
+                )
+            # also, record the best p-value among all the SNPs with this allele
+            best_p_idx[allele] = results[allele].data["pval"].argmin()
+        # exit if neither of the alleles worked
+        if not len(best_p_idx):
+            return
+        # step 3: find the index of the best variant within the haplotype matrix
+        best_allele = min(
+            best_p_idx, key=lambda a: results[a].data["pval"][best_p_idx[a]]
+        )
+        best_var_idx = best_p_idx[best_allele]
+        num_tests = len(parent.nodes) + 1
+        # step 4: find the index of the best variant within the genotype matrix
+        # we need to account for indices that we removed when running transform()
+        # There might be a faster way of doing this but for now we're just going to
+        # live with it
+        for gt_idx in sorted(parent.node_indices):
+            # add each idx back in, so long as they are less than the target idx
+            if gt_idx > best_var_idx:
+                break
+            best_var_idx += 1
+        # step 5: retrieve the Variant with the best p-value
+        best_variant = Variant.from_np(self.gens.variants[best_var_idx], best_var_idx)
+        self.log.debug("Chose variant {}".format(best_variant.id))
+        # iterate through all of the alleles of the best variant and check if they're
+        # significant
+        for allele in alleles:
+            best_results = results[allele].data[best_p_idx[best_allele]]
+            node_res = self.results_type.from_np(best_results)
+            # step 6: check whether we should terminate the branch
             self.log.debug(
                 "Testing variant {} / allele {} with parent_res {} and node_res {}"
                 .format(best_variant.id, allele, parent_res, node_res)
