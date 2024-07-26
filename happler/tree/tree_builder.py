633138297b1d3629ac9800f5597e39f752467fe5
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2024-07-26 06:06:00 +0000

    try again to refmt with black

--- happler/tree/tree_builder.py
+++ happler/tree/tree_builder.py
@@ -331,8 +331,9 @@ class TreeBuilder:
                 )
             # step 7: check if this allele is significant and whether we should terminate the branch
             self.log.debug(
-                "Testing variant {} / allele {} with parent_res {} and node_res {}"
-                .format(best_variant.id, allele, parent_res, node_res)
+                "Testing variant {} / allele {} with parent_res {} and node_res {}".format(
+                    best_variant.id, allele, parent_res, node_res
+                )
             )
             if self.terminator.check(
                 parent_res, node_res, results, best_res_idx, num_samps, num_tests
@@ -487,8 +488,9 @@ class TreeBuilder:
             node_res = self.results_type.from_np(best_results)
             # step 8: check whether we should terminate the branch
             self.log.debug(
-                "Testing variant {} / allele {} with parent_res {} and node_res {}"
-                .format(best_variant.id, allele, parent_res, node_res)
+                "Testing variant {} / allele {} with parent_res {} and node_res {}".format(
+                    best_variant.id, allele, parent_res, node_res
+                )
             )
             if self.terminator.check(
                 parent_res,
