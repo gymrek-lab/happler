d8af216866867e2a90e6e54b7afc63e0bc5a2dbe
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2024-08-22 13:22:29 -0700

    display r-squared in explained_variance_plot

--- analysis/workflow/scripts/variance_explained_plot.py
+++ analysis/workflow/scripts/variance_explained_plot.py
@@ -32,9 +32,15 @@ def standardize(data):
     return new_data
 
 
+def get_beta_and_rsquared(result: sm.regression.linear_model.RegressionResults):
+    # TODO: should we return adjusted r-squared instead?
+    return result.params[0], result.rsquared
+
+
 def compute_explained_variance(gt: npt.NDArray, pt: npt.NDArray):
     """
     Compute explained variance for each SNP or haplotype in a set
+    Also compute R-squared values
 
     The explained variance is beta^2 in a linear model y = bx + e when x and y have
     been standardized to mean 0 and stdev 1
@@ -46,21 +52,48 @@ def compute_explained_variance(gt: npt.NDArray, pt: npt.NDArray):
         num_samples by num_variants
     pt: npt.NDArray
         The phenotype as a 1D array
-    
+
     Returns
     -------
     float
-        The explained variance of the SNPs/haplotypes
+        The explained variance of the SNPs/haplotypes and the R squared values
     """
     # standardize the phenotypes and genotypes
     pt = standardize(pt[:, np.newaxis]).flatten()
     gt = standardize(gt)
     # compute the betas of each SNP by fitting a linear model y = beta * x + e
-    betas = np.array([
-        sm.OLS(pt, gt[:, snp_idx].flatten()).fit().params[0]
+    betas_rsquared = np.array([
+        get_beta_and_rsquared(sm.OLS(pt, gt[:, snp_idx].flatten()).fit())
         for snp_idx in range(gt.shape[1])
     ])
-    return betas**2
+    # compute explained variance by squaring the betas
+    betas_rsquared[:, 0] = betas_rsquared[:, 0]**2
+    return betas_rsquared
+
+
+def compute_multisnp_rsquared(gt: npt.NDArray, pt: npt.NDArray):
+    """
+    Compute the R-squared value for SNPs (from a single haplotype) in a multiple linear
+    regression
+
+    Parameters
+    ----------
+    gt: npt.NDArray
+        The genotypes of the SNPs in a haplotype as a 2D array of shape:
+        num_samples by num_variants
+    pt: npt.NDArray
+        The phenotype as a 1D array
+
+    Returns
+    -------
+    float
+        The R-squared value of the SNPs from a haplotype
+    """
+    # standardize the phenotypes and genotypes
+    pt = standardize(pt[:, np.newaxis]).flatten()
+    gt = standardize(gt)
+    # fitting a linear model y = beta1 * x1 + beta2 * x2 + ... + e
+    return sm.OLS(pt, gt).fit().rsquared
 
 
 def get_explained_variances(
@@ -124,13 +157,24 @@ def get_explained_variances(
     )))
 
     # create a dictionary mapping hap IDs to two-element tuples containing
-    # explained variances for 1) the haplotype and 2) the sum of the haplotype's SNPs
-    explained_variances = {}
+    # 1) explained variance for the haplotype
+    # 2) R-squared for the haplotype
+    # 3) explained variance for the haplotype's SNPs
+    # 4) R-squared for the haplotype's SNPs
+    vals = {}
     for hp in hps.data.values():
-        sum_of_SNPs = sum(raw_explained_variances[v.id] for v in hp.variants)
-        explained_variances[hp.id] = (raw_explained_variances[hp.id], sum_of_SNPs)
+        sum_of_SNPs = sum(raw_explained_variances[v.id][0] for v in hp.variants)
+        multi_rsquared = compute_multisnp_rsquared(
+            gts.subset(variants=tuple(v.id for v in hp.variants)).data.sum(axis=2),
+            pts.data[:, 0],
+        )
+        vals[hp.id] = (
+            *raw_explained_variances[hp.id],
+            sum_of_SNPs,
+            multi_rsquared,
+        )
 
-    return explained_variances
+    return vals
 
 
 @click.command()
@@ -198,10 +242,10 @@ def main(
         params = params[subset_mask]
 
     # compute explained variance for each haplotype and its SNPs
-    # this 2D array should have two columns: 1) the haplotype and 2) its SNPs
+    # this 2D array should have two*2 columns: 1) the haplotype and 2) its SNPs
     # and should have as many rows as there are haplotypes among all of the loci
     # (note that some loci may have multiple haplotypes)
-    explained_variances = np.array([
+    vals = np.array([
         hap
         for idx in range(len(params))
         for hap in get_explained_variances(
@@ -211,10 +255,12 @@ def main(
             log=log
         ).values()
     ])
+    explained_variances = vals[:, (0, 2)]
+    rsquareds = vals[:, (1, 3)]
 
     if np.any(explained_variances > 1):
         log.error(
-            "Some of the explained variances is greater than 1! Check that nothing "
+            "Some of the explained variances are greater than 1! Check that nothing "
             "went wrong."
         )
 
@@ -222,25 +268,52 @@ def main(
     percent_success = 100*(
         explained_variances[:, 1] < explained_variances[:, 0]
     ).sum()/explained_variances.shape[0]
-    log.warning(
+    log.info(
         f"{percent_success:.3f}% of haplotypes explain more phenotypic variation than"
         " their SNPs"
     )
     percent_success = 100*(
         explained_variances[:, 1] == explained_variances[:, 0]
     ).sum()/explained_variances.shape[0]
-    log.warning(
+    log.info(
         f"{percent_success:.3f}% of haplotypes explain as much phenotypic variation as"
         " their SNPs"
     )
+    # how good are we doing?
+    percent_success = 100*(
+        rsquareds[:, 1] < rsquareds[:, 0]
+    ).sum()/rsquareds.shape[0]
+    log.info(
+        f"{percent_success:.3f}% of haplotypes explain more R-squared than"
+        " their SNPs"
+    )
+    percent_success = 100*(
+        rsquareds[:, 1] == rsquareds[:, 0]
+    ).sum()/rsquareds.shape[0]
+    log.info(
+        f"{percent_success:.3f}% of haplotypes explain as much R-squared as"
+        " their SNPs"
+    )
 
-    max_val = explained_variances.max()
+    max_ev_val = explained_variances.max()
+    max_r2_val = rsquareds.max()
 
-    plt.scatter(explained_variances[:, 1], explained_variances[:, 0])
-    plt.axline([0, 0], [max_val, max_val])
-    plt.title("Variance Explained")
-    plt.xlabel("Haplotype's SNPs")
-    plt.ylabel("Haplotype")
+    f, (ax1, ax2) = plt.subplots(1, 2)
+
+    ax1.scatter(explained_variances[:, 1], explained_variances[:, 0])
+    ax1.axline([0, 0], [max_ev_val, max_ev_val])
+    ax1.set_title("Variance Explained")
+    ax1.set_xlabel("Haplotype's SNPs")
+    ax1.set_ylabel("Haplotype")
+
+    ax2.scatter(rsquareds[:, 1], rsquareds[:, 0])
+    ax2.axline([0, 0], [max_r2_val, max_r2_val])
+    ax2.set_title("R-Squared")
+    ax2.set_xlabel("Haplotype's SNPs")
+    ax2.set_ylabel("Haplotype")
+
+    f.set_size_inches(10, 5)
+    plt.tight_layout()
     plt.savefig(output)
 
 
