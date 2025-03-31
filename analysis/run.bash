d43eba4adb79d9aa550d51e64f04807cd06b7645
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2025-03-31 13:42:47 -0700

    revise run.bash to warn during timeout

--- analysis/run.bash
+++ analysis/run.bash
@@ -10,22 +10,23 @@
 #SBATCH --time 0:30:00
 #SBATCH --mem 2G
 #SBATCH --output /dev/null
-#SBATCH --signal=B:SIGUSR1@15
+#SBATCH --signal=B:SIGUSR1@5
 
-trap "slack job terminating early" SIGUSR1
 
 # An example bash script demonstrating how to run the entire snakemake pipeline
-# This script creates two separate log files in the output dir:
-# 	1) log - the basic snakemake log of completed rules
+# This script creates a log file in the execution directory
 
 # Before running this snakemake pipeline, remember to complete the config file
 # with the required input info.
 # Also, make sure that this script is executed from the directory that it lives in!
 
+# Before we do anything, let's ensure that we get notified in case this job times out
+trap "command -v slack &>/dev/null && slack \"TIMEOUT: snakemake job\"" SIGUSR1
+
 out_path="out"
 mkdir -p "$out_path"
 
-# clear leftover log files
+# Clear leftover log files
 echo ""> "$out_path/log"
 
 # try to find and activate the snakemake conda env if we need it
@@ -39,7 +40,7 @@ if ! command -v 'snakemake' &>/dev/null && \
 fi
 
 
-# check: are we being executed from within qsub?
+# Check: Are we within a SLURM batch job or just an interactive node?
 if [ "$ENVIRONMENT" = "BATCH" ]; then
     snakemake \
     --workflow-profile profile/slurm \
@@ -48,7 +49,7 @@ if [ "$ENVIRONMENT" = "BATCH" ]; then
     -k \
     -j 32 \
     -c 32 \
-    "$@" &>"$out_path/log"
+    "$@" &>"$out_path/log" &
 else
     snakemake \
     --workflow-profile profile/default \
@@ -56,11 +57,13 @@ else
     --notemp \
     -k \
     -c 4 \
-    "$@" &>"$out_path/log"
+    "$@" &>"$out_path/log" &
 fi
 
-# send a slack message to notify that the job has finished
-exit_code="$?"
+wait $!
+exit_code=$?
+
+# Send a slack message to notify us that the job completed
 if command -v 'slack' &>/dev/null; then
     if [ "$exit_code" -eq 0 ]; then
         slack "snakemake finished successfully" &>/dev/null
