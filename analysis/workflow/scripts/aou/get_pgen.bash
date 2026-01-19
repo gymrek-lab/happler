7e2697ecc68b61dad8aa9e57a24b2180919f4e4c
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2026-01-19 15:28:56 -0800

    oops update prefix path again

--- analysis/workflow/scripts/aou/get_pgen.bash
+++ analysis/workflow/scripts/aou/get_pgen.bash
@@ -41,5 +41,5 @@ for batch in $batches; do
 done
 
 cd -
-bash "$(dirname "$0")"/merge_batched_vcfs.bash "$TEMP_DIR/$region.vcf.gz" "$TEMP_DIR/$region"
-plink2 --threads "$threads" --out "$out_prefix" --nonfounders --vcf "$TEMP_DIR/$region.vcf.gz" --geno 0 --make-pgen --allow-extra-chr --max-alleles 2 --chr "$chrom" --from-bp "$pos" --to-bp "$end"
+bash "$(dirname "$0")"/merge_batched_vcfs.bash "$out_prefix".vcf.gz "$TEMP_DIR/$region"
+plink2 --threads "$threads" --out "$out_prefix" --nonfounders --vcf "$out_prefix".vcf.gz --geno 0 --make-pgen --allow-extra-chr --max-alleles 2 --chr "$chrom" --from-bp "$pos" --to-bp "$end"
