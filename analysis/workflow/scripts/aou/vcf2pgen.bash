f1f152c08ffa8339267c83445603bacfbb61c3f5
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2026-01-30 20:28:41 +0000

    reverse chrom order and fix plink2 command

--- analysis/workflow/scripts/aou/vcf2pgen.bash
+++ analysis/workflow/scripts/aou/vcf2pgen.bash
@@ -10,9 +10,9 @@ V8_CDR_DIR="gs://fc-aou-datasets-controlled/v8/wgs/short_read/snpindel/aux/phasi
 mkdir -p ~/phased_pgens
 cd ~/phased_pgens
 
-for i in {1..22}; do
+for i in {22..1}; do
     gsutil -u $GOOGLE_PROJECT -m cp -r "$V8_CDR_DIR"/chr${i}_*.vcf.gz chr${i}.vcf.gz && \
-    plink2 --memory 24000 --vcf chr${i}.vcf.gz --out chr${i} --set-all-var-ids '@:#' && \
+    plink2 --memory 24000 --vcf chr${i}.vcf.gz --out chr${i} --set-all-var-ids '@:#' --make-just-pvar --keep-autoconv && \
     rm chr${i}.vcf.gz;
     (
         gsutil cp chr${i}.log chr${i}.p* "$V8_BUCKET"/phased_pgens/ && \
