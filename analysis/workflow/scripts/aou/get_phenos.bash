77155060105f898275666479820dceeda4abcc9b
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2026-02-03 10:15:21 -0800

    use v8 in get_phenos

--- analysis/workflow/scripts/aou/get_phenos.bash
+++ analysis/workflow/scripts/aou/get_phenos.bash
@@ -2,7 +2,7 @@
 
 export GCS_REQUESTER_PAYS_PROJECT="${GOOGLE_PROJECT}"
 export GCS_OAUTH_TOKEN="$(gcloud auth application-default print-access-token)"
-CDR_DIR="gs://fc-aou-datasets-controlled/v7"
+CDR_DIR="gs://fc-aou-datasets-controlled/v8"
 
 mkdir -p phenos
 cd phenos
@@ -25,5 +25,5 @@ for pheno in platelet_count_new_phenocovar ldl_cholesterol_phenocovar; do
     cut -f 1,3- -d, --output-delimiter $'\t' $pheno.csv | { read -r head; echo "$head" | sed 's/person_id/#IID/'; cat; } > $pheno.og.covar
     # we use the first 10 PCs and the first column is #IID, so we do -f-11
     ../happler/analysis/workflow/scripts/residuals.py -o $pheno.residuals.pheno -e <(cut -f-11 AOU_PCS.covar) $pheno.og.pheno $pheno.og.covar
-    gsutil cp $pheno.residuals.pheno ${WORKSPACE_BUCKET}/aryarm/phenos/
+    gsutil cp $pheno.residuals.pheno ${WORKSPACE_BUCKET}/aryarm/data/aou/phenos/
 done
