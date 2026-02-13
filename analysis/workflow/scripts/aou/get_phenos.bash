ebf30c427e589d317fa298d7298a184b4d45e9c4
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2026-02-13 13:36:22 -0800

    try to fix bug in manhattan.py when passing multiple linear files

--- analysis/workflow/scripts/aou/get_phenos.bash
+++ analysis/workflow/scripts/aou/get_phenos.bash
@@ -23,11 +23,13 @@ tail -n+2 | {
 } | \
 cut --complement -f2 > AOU_PCS.covar
 
-for pheno in platelet_count_phenocovar ldl_cholesterol_phenocovar; do
+for pheno in platelet_count_new_phenocovar ldl_cholesterol_phenocovar; do
     gcloud storage cp ${WORKSPACE_BUCKET}/phenotypes/$pheno.csv $pheno.csv
     cut -f-2 -d, --output-delimiter $'\t' $pheno.csv | { echo -e "#IID\tpheno"; tail -n+2; } > $pheno.og.pheno
     cut -f 1,3- -d, --output-delimiter $'\t' $pheno.csv | { read -r head; echo "$head" | sed 's/person_id/#IID/'; cat; } > $pheno.og.covar
     # we use the first 10 PCs and the first column is #IID, so we do -f-11
     ../happler/analysis/workflow/scripts/residuals.py -o $pheno.residuals.pheno -e <(cut -f-11 AOU_PCS.covar) $pheno.og.pheno $pheno.og.covar
+    # Note that we passed both .covar files. If we wanted to merge the .covar files instead, we could do it like this:
+    # join -j 1 -t $'\t' --header <(cat $pheno.og.covar | (sed-u 1q; sort -k1,1)) <(cat AOU_PCS.covar | cut -f-11 | (sed -u 1q; sort -k1,1)) > $pheno.merged.og.covar
     gcloud storage cp $pheno.residuals.pheno ${WORKSPACE_BUCKET}/aryarm/data/aou/phenos/$pheno.resid.pheno
 done
