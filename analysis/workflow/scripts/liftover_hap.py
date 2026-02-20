a23d6910c3e293a8a70dbfdbd07623e01ca4b1b8
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2026-02-20 23:28:56 +0000

    add liftover script for hap files

---
+++ analysis/workflow/scripts/liftover_hap.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+
+import click
+from liftover import ChainFile
+
+
+@click.command()
+@click.argument("hap", type=click.Path(exists=True, path_type=Path))
+@click.argument("chain", type=click.Path(exists=True, path_type=Path))
+@click.option(
+    "--update-varIDs",
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help="Also update the variant IDs to use CHROM:POS format",
+)
+@click.option(
+    "-o",
+    "--output",
+    type=click.Path(path_type=Path),
+    default=Path("/dev/stdout"),
+    show_default="stdout",
+    help="A haplotypes file with its coordinates lifted over",
+)
+def main(
+    hap: Path,
+    chain: Path,
+    update_varids: bool = False,
+    output: Path = Path("/dev/stdout"),
+):
+    """
+    Lifts over the coordinates in a .hap file using a .chain.gz file.
+    """
+
+    converter = ChainFile(chain, one_based=True)
+
+    with open(hap, "r") as hap_file:
+        with open(output, "w") as out_file:
+            chrom = None
+            # Iterate over each line in the input file
+            for line in hap_file:
+                first_char = line[0]
+                line = line.strip("\n")
+                if not first_char == "#" and (
+                    first_char == "H" or first_char == "V" or first_char == "R"
+                ):
+                    line_parts = line.split("\t")
+                    if first_char == "H":
+                        chrom = line_parts[1]
+                    line_parts[2] = str(converter[chrom][int(line_parts[2]) - 1][0][1])
+                    line_parts[3] = str(converter[chrom][int(line_parts[3]) - 1][0][1])
+                    if update_varids and (first_char == "V" or first_char == "R"):
+                        line_parts[4] = f"{chrom}:{line_parts[2]}"
+                    line = "\t".join(line_parts)
+                out_file.write(line + "\n")
+
+
+if __name__ == "__main__":
+    main()
