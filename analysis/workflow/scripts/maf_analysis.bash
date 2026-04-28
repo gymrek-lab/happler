c36af8b66dac78a519eb1677525f9c9bd1ab11a8
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2026-04-28 16:22:46 -0700

    bump threshold back to 20

--- analysis/workflow/scripts/maf_analysis.bash
+++ analysis/workflow/scripts/maf_analysis.bash
@@ -2,7 +2,7 @@
 
 # arg1: the ID of the causal SNP (ex: 5:88884379)
 # arg1: region to extract (ex: 5_87367336-90059999)
-# arg2: MAFs (ex: "0.001 0.01 0.015 0.02 0.025 0.03 0.035 0.04 0.045 0.05 0.1")
+# arg2: MAFs (ex: "0.001 0.01 0.015 0.02 0.025 0.03 0.035 0.04 0.045 0.05 0.065 0.085 0.1")
 # arg3: phenotype file (ex: data/aou/phenos/platelet_count.resid.pheno)
 # arg4: out path (ex: out)
 # ex: workflow/scripts/maf_analysis.bash 5:88884379 5_87367336-90059999 "0.05 0.055 0.1" data/aou/phenos/platelet_count.resid.pheno out
@@ -26,7 +26,6 @@ geno_file="$out/$region"/genotypes/"$pheno_name"/"$geno_name"
 
 mkdir -p "$output_dir"
 
-# NOTE: THRESHOLD IS 18 NOT 20
 for maf in "${mafs[@]}"; do
     [ ! -f "$output_dir/$maf".hap ] && \
     echo "Running happler for MAF "$maf 1>&2 && \
@@ -40,7 +39,7 @@ for maf in "${mafs[@]}"; do
     --discard-multiallelic \
     --remove-SNPs \
     --indep-thresh 15 \
-    -t 18 \
+    -t 20 \
     --chunk-size 500 \
     --out-thresh 5e-08 \
     "$geno_file".pgen \
@@ -101,21 +100,19 @@ set -o pipefail
 
 echo "Merging the SNP and hap r2 reports together" 1>&2
 {
-    echo -e "maf_thresh\thap_id\thap_r2\tsnp_r2"
+    echo -e "maf_thresh\thap_id\thap_r2\tsnp_r2\tsnp_maf"
     join -t $'\t' -a1 -11 -22 <(
-        tail -n+2 "$output_dir/$best_variant"/snps.ld | cut -f 1,4 | sort -k1,1
+        tail -n+2 "$output_dir/$best_variant"/snps.ld | cut -f 1,3,4 | sort -k1,1
     ) <(
         tail -n+2 "$output_dir/$best_variant"/haps.ld | cut -f3,4 | sed 's/:/\t/' | sort -k2,2
     ) | \
-    awk -F $'\t' -v 'OFS=\t' 'NF == 2 { $3="H0";$4=$2 } {print $1, $3, $4, $2;}'
+    awk -F $'\t' -v 'OFS=\t' 'NF == 3 { $4="H0";$5=$3 } {print $1, $4, $5, $3, $2;}'
 } > "$output_dir/$best_variant"/maf_hap_snp_r2.tsv
 
 # -------------
 
-cd "$output_dir"
-
 echo "Plotting variance explained" 1>&2
-(
+( cd "$output_dir" && (
   echo "a=["$(cut -f1,6 variance_explained.tsv | tail -n+2 | tr ':' $'\t' | grep 'H0' | cut -f 1,3 | sort -g | tr $'\t' , | sed 's/^/(/;s/$/)/' | paste -s -d,)"]"
   cat <<'EOF'
 import numpy as np
@@ -130,13 +127,11 @@ plt.title("Variance Explained (R^2)")
 plt.savefig("varexp_maf.png")
 
 EOF
-) | python
-
-cd "$best_variant"
+) | python; )
 
 echo "Plotting LD with causal variant" 1>&2
-(
-  echo "a=["$(tail -n+2 maf_hap_snp_r2.tsv | sed 's/\tH0\t/\t0\t/;s/\tH1\t/\t1\t/' | sort -t$'\t' -k1,1g | tr $'\t' , | sed 's/^/(/;s/$/)/' | paste -s -d,)"]"
+( cd "$output_dir/$best_variant" && (
+  echo "causal_variant=\"$best_variant\"; a=["$(tail -n+2 maf_hap_snp_r2.tsv | sed 's/\tH0\t/\t0\t/;s/\tH1\t/\t1\t/' | sort -t$'\t' -k1,1g | tr $'\t' , | sed 's/^/(/;s/$/)/' | paste -s -d,)"]"
   cat <<'EOF'
 import numpy as np
 import matplotlib
@@ -145,15 +140,20 @@ import matplotlib.pyplot as plt
 data = np.array(a)
 both = data[data[:,2] == data[:,3]]
 hap_ids = np.unique(data[:,1])
+mafs = np.unique(data[:,4])
 for hp_id in hap_ids:
   dat = data[hp_id == data[:,1]]
   plt.plot(dat[:,0], dat[:,2], 'o-', label=f"Haplotype {int(hp_id)}")
 plt.plot(data[:,0], data[:,3], 'o-', label="Best SNP")
 plt.plot(both[:,0], both[:,3], 'o', label="Both")
+for maf in mafs:
+  plt.axvline(x=maf, color='red', linestyle='--')
 plt.xlabel("MAF")
 plt.ylabel("LD (R^2) with causal SNP")
+plt.ylim(0, 1.02)
+plt.title(causal_variant)
 plt.legend()
 plt.savefig("ld_maf.png")
 
 EOF
-) | python
+) | python; )
