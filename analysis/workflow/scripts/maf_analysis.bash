3320505f2edc10c54402822667cb5cfd939f6ef1
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2026-05-02 20:40:40 -0700

    try again with missing region arg

--- analysis/workflow/scripts/maf_analysis.bash
+++ analysis/workflow/scripts/maf_analysis.bash
@@ -80,7 +80,7 @@ for maf in "${all_mafs[@]}"; do
     conda activate .snakemake/conda/885b27680699bdbf0ec4008de1a842a3_
     [ ! -f "$output_dir/$maf.rds" ] && \
     echo "Running SuSiE for MAF $maf" 1>&2 && \
-    workflow/scripts/run_SuSiE.R "$output_dir/$maf".merge.pgen "$pheno" "$output_dir" NULL 10 &> "$output_dir/$maf".susie.log && \
+    workflow/scripts/run_SuSiE.R "$output_dir/$maf".merge.pgen "$pheno" "$output_dir" NULL "$(echo "$region" | sed 's/_/:/')" 10 &> "$output_dir/$maf".susie.log && \
     workflow/scripts/extract_pips.R "$output_dir/$maf.rds" "$output_dir/$maf.pips.tsv" &>"$output_dir/$maf.pips.log"
     conda deactivate
 done
