ac6d39bc81d80502af9f7464d928012a99ae56f8
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2026-08-02 10:29:47 -0700

    clean up pip.ext plot

--- analysis/workflow/scripts/pip.ext.ex-vs-in.py
+++ analysis/workflow/scripts/pip.ext.ex-vs-in.py
@@ -7,6 +7,8 @@ from haptools.logging import getLogger
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from matplotlib.patches import ConnectionPatch
 
 
 @click.command()
@@ -28,6 +30,20 @@ import matplotlib.pyplot as plt
     help="Subset to points with this beta",
 )
 @click.option(
+    "--include-raw-scatter",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Should we include a scatterplot containing the raw hap PIPs?",
+)
+@click.option(
+    "--combine-percent-plots",
+    is_flag=True,
+    show_default=True,
+    default=False,
+    help="Should we try to combine the PIP percent plots?",
+)
+@click.option(
     "-o",
     "--output",
     type=click.Path(path_type=Path),
@@ -47,6 +63,8 @@ def main(
     pips_file: Path,
     sampsize: int = None,
     beta: int = None,
+    include_raw_scatter: bool = False,
+    combine_percent_plots: bool = False,
     output: Path = None,
     verbosity: str = "INFO"
 ):
@@ -71,7 +89,7 @@ def main(
         data = data[passing]
 
     # Sort by delta_bic_train
-    delta_bic_thresh = 25
+    delta_bic_thresh = 20
     data.sort(order='delta_bic_train')
 
     # Calculate percent of points above 0.5 for the bottom row
@@ -80,8 +98,19 @@ def main(
     # hap_pip_train_pct_above = data['hap_pip_train']
     # hap_pip_test_pct_above = data['hap_pip_test']
 
-    # Create figure with 2x2 subplots
-    fig, axes = plt.subplots(3, 2, figsize=(9.5, 12))
+    # Create figure with 2x2 subplots or 1x3 of combine_percent_plots is true
+    # If include_raw_scatter is true, then we add one more row with two axes
+    fig, axes = plt.subplots(
+        2+int(include_raw_scatter)-int(combine_percent_plots), # height
+        2+int(combine_percent_plots), # width
+        figsize=(
+            4.75*(2+int(combine_percent_plots)), # width
+            4*(2+int(include_raw_scatter)-int(combine_percent_plots)), # height
+        ),
+    )
+
+    if axes.ndim <= 1:
+        axes = axes[np.newaxis, :]
 
     # Plot 1: include_pip_train vs exclude_pip_train
     axes[0, 0].scatter(data['exclude_pip_train'], data['include_pip_train'], alpha=0.6)
@@ -94,8 +123,9 @@ def main(
     #     'exclude_pip_train',
     #     'include_pip_train',
     # )
-    axes[0, 0].set_xlabel("exclude_pip_train")
-    axes[0, 0].set_ylabel("include_pip_train")
+    axes[0, 0].set_xlabel("PIP of causal parent SNP when haplotype is excluded")
+    axes[0, 0].set_ylabel("PIP of causal parent SNP when haplotype is included")
+    axes[0, 0].set_title("Training Cohort")
     axes[0, 0].grid(True, alpha=0.3)
 
     # Plot 2: include_pip_test vs exclude_pip_test
@@ -109,41 +139,82 @@ def main(
     #     'exclude_pip_test',
     #     'include_pip_train',
     # )
-    axes[0, 1].set_xlabel("exclude_pip_test")
-    axes[0, 1].set_ylabel("include_pip_test")
+    axes[0, 1].set_xlabel("PIP of causal parent SNP when haplotype is excluded")
+    axes[0, 1].set_ylabel("PIP of causal parent SNP when haplotype is included")
+    axes[0, 1].set_title("Validation Cohort")
     axes[0, 1].grid(True, alpha=0.3)
 
-    # Plot 3: hap_pip_train vs delta_bic_train
-    axes[1, 0].scatter(data['delta_bic_train'], data['hap_pip_train'], alpha=0.6)
-    axes[1, 0].set_xlabel('delta_bic_train')
-    axes[1, 0].set_ylabel('hap_pip_train_percent_above_0.5')
-    axes[1, 0].axvline(x=delta_bic_thresh, color="red")
-    axes[1, 0].grid(True, alpha=0.3)
+    if include_raw_scatter:
+        # Plot 3: hap_pip_train vs delta_bic_train
+        axes[1, 0].scatter(data['delta_bic_train'], data['hap_pip_train'], alpha=0.6)
+        axes[1, 0].set_xlabel('Delta BIC in training cohort')
+        axes[1, 0].set_ylabel('Non-causal haplotype PIPs')
+        axes[1, 0].axvline(x=delta_bic_thresh, color="red")
+        axes[1, 0].grid(True, alpha=0.3)
 
-    # Plot 4: hap_pip_test vs delta_bic_train
-    axes[1, 1].scatter(data['delta_bic_train'], data['hap_pip_test'], alpha=0.6)
-    axes[1, 1].set_xlabel('delta_bic_train')
-    axes[1, 1].set_ylabel('hap_pip_test_percent_above_0.5')
-    axes[1, 1].axvline(x=delta_bic_thresh, color="red")
-    axes[1, 1].grid(True, alpha=0.3)
+        # Plot 4: hap_pip_test vs delta_bic_train
+        axes[1, 1].scatter(data['delta_bic_train'], data['hap_pip_test'], alpha=0.6)
+        axes[1, 1].set_xlabel('Delta BIC in training cohort')
+        axes[1, 1].set_ylabel('Non-causal haplotype PIPs')
+        axes[1, 1].axvline(x=delta_bic_thresh, color="red")
+        axes[1, 1].grid(True, alpha=0.3)
 
-    # Plot 5: hap_pip_train_percent vs delta_bic_train
-    axes[2, 0].plot(data['delta_bic_train'], hap_pip_train_pct_above)
-    axes[2, 0].set_xlabel('delta_bic_train')
-    axes[2, 0].set_ylabel('hap_pip_train_percent_above_0.5')
-    axes[2, 0].axvline(x=delta_bic_thresh, color="red")
-    axes[2, 0].grid(True, alpha=0.3)
+    if combine_percent_plots:
+        axes[0, 2].plot(data['delta_bic_train'], hap_pip_test_pct_above, label="Validation cohort", color="orange")
+        axes[0, 2].plot(data['delta_bic_train'], hap_pip_train_pct_above, label="Training cohort", color="blue")
+        axes[0, 2].set_xlabel('Delta BIC in training cohort')
+        axes[0, 2].set_ylabel('Percent of non-causal haplotype PIPs > 0.5')
+        axes[0, 2].axvline(x=delta_bic_thresh, color="red", label=f"delta BIC threshold: ({delta_bic_thresh})")
+        axes[0, 2].grid(True, alpha=0.3)
+        axes[0, 2].legend(loc="upper right")
 
-    # Plot 6: hap_pip_test_percent vs delta_bic_train
-    axes[2, 1].plot(data['delta_bic_train'], hap_pip_test_pct_above)
-    axes[2, 1].set_xlabel('delta_bic_train')
-    axes[2, 1].set_ylabel('hap_pip_test_percent_above_0.5')
-    axes[2, 1].axvline(x=delta_bic_thresh, color="red", label=f"threshold: ({delta_bic_thresh})")
-    axes[2, 1].grid(True, alpha=0.3)
+        if include_raw_scatter:
+            axes[1, 2].set_axis_off()
+    else:
+        # Plot 5: hap_pip_train_percent vs delta_bic_train
+        axes[1+int(include_raw_scatter), 0].plot(data['delta_bic_train'], hap_pip_train_pct_above)
+        axes[1+int(include_raw_scatter), 0].set_xlabel('Delta BIC in training cohort')
+        axes[1+int(include_raw_scatter), 0].set_ylabel('Percent of non-causal haplotype PIPs > 0.5')
+        axes[1+int(include_raw_scatter), 0].axvline(x=delta_bic_thresh, color="red")
+        axes[1+int(include_raw_scatter), 0].grid(True, alpha=0.3)
+
+        # Plot 6: hap_pip_test_percent vs delta_bic_train
+        axes[1+int(include_raw_scatter), 1].plot(data['delta_bic_train'], hap_pip_test_pct_above)
+        axes[1+int(include_raw_scatter), 1].set_xlabel('Delta BIC in training cohort')
+        axes[1+int(include_raw_scatter), 1].set_ylabel('Percent of non-causal haplotype PIPs > 0.5')
+        axes[1+int(include_raw_scatter), 1].axvline(x=delta_bic_thresh, color="red", label=f"Delta BIC threshold: {delta_bic_thresh}")
+        axes[1+int(include_raw_scatter), 1].grid(True, alpha=0.3)
+        axes[1+int(include_raw_scatter), 1].set_ylim(bottom=-0.05)
+
+        minor_tick_val = axes[1+int(include_raw_scatter), 1].get_yticks().max()
+
+        # add y-axis tick mark to the hap_pip_train plot at the max tick mark of the hap_pip_test plot
+        # axes[1+int(include_raw_scatter), 0].yaxis.set_minor_locator(ticker.FixedLocator([minor_tick_val]))
+
+        ax1 = axes[1+int(include_raw_scatter), 0]
+        ax2 = axes[1+int(include_raw_scatter), 1]
+        con1 = ConnectionPatch(
+            xyA=(0, minor_tick_val), coordsA=ax1.get_yaxis_transform(),
+            xyB=(0, 1.0), coordsB=ax2.transAxes,
+            axesA=ax1, axesB=ax2,
+            color="gray",
+            linestyle="--",
+            linewidth=1.5,
+        )
+        con2 = ConnectionPatch(
+            xyA=(0, -0.05), coordsA=ax1.get_yaxis_transform(),
+            xyB=(0, 0), coordsB=ax2.transAxes,
+            axesA=ax1, axesB=ax2,
+            color="gray",
+            linestyle="--",
+            linewidth=1.5,
+        )
+        fig.add_artist(con1)
+        fig.add_artist(con2)
+        fig.legend(loc="lower center")
 
     # Adjust layout to prevent overlap
     plt.tight_layout()
-    fig.legend(loc="lower right")
 
     # Save and show the plot
     plt.savefig(output, dpi=300, bbox_inches='tight')
